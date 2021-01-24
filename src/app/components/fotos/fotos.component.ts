d20da3cd132fdcecf9e83c0177291ceff02c1be4
Albert <johan50693@gmail.com>
2021-01-24 13:46:19 -0400

    Se carga el listado de imagenes cargadas, fin de app para carga de imagenes

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+export interface Item { nombre: string, url: string }
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotosComponent implements OnInit {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('img');
+    this.items = this.itemsCollection.valueChanges();
+  }
 
   ngOnInit() {
   }
